3e75a97d9968dc155f636ce7b9a8c496bdd1d3db
itsdfish <itsdfish@gmail.com>
2025-07-14 18:02:01 -0400

    breaking version

--- src/TuringUtilities.jl
+++ src/TuringUtilities.jl
@@ -6,17 +6,23 @@ using Turing: @model
 export predict_distribution
 
 """
-    predict_distribution(dist, args...; model, func, n_samples, kwargs...)
+    function predict_distribution(
+        args...;
+        simulator,
+        model,
+        func = (x, args...; kwargs...) -> x,
+        kwargs...
+    )
 
 Generates a predictive distribution for a statistic defined by `func`.
 
 # Arguments 
 
-- `dist::Distribution`: a distribution type which accepts parameters as keyword arguments 
 - `args...` optional positional arguments passed to function `func`
 
 # Keywords
 
+- `simulator`: a function that accepts parameters and returns simulated data 
 - `model`: a Turing model which returns a `NamedTuple` of parameters 
 - `func`: a function which computes a statistic of simulated data. The function signature is `func(data, args...; kwargs...)`
 - `n_samples`: the number of observations to sample from `dist`
